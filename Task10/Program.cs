57bee6df5fc64f913d04eafce9e06b95c3380c47
Andrey <tishok@inbox.ru>
2023-01-23 15:14:28 +0300

    Сделал Таск 10 но не знаю как сделать так чтобы вводилось только трехзначное число и выводилась надпись с ошибкой если число не трехзначное

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//  Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+Console.WriteLine("Введите трехзначное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Вторая цифра трехзначного числа = ");
+Console.WriteLine(centerNumber(number));
+
+int centerNumber ( int a) {
+   int result = ((a/10) % 10);
+    return result;
+}
